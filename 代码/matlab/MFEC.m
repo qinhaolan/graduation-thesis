77bee60c93e8ac02598bb03f2a1abcc02e80fd23
qhl@win10 <2730176095@qq.com>
2021-05-02 17:10:49 +0800

    重构完第三章

--- 代码/matlab/MFEC.m
+++ 代码/matlab/MFEC.m
@@ -1,22 +1,22 @@
 %模糊最大熵聚类
-function [U,V,C] = MFEC(data, c)
+function [A,V,C] = MFEC(data, c)
 T=1000;
 epsm=0.0001;
 [n, s] = size(data);
 
 % 初始化隶属度矩阵并归一化
-U= rand(c, n);
-temp = sum(U,1);
+A= rand(c, n);
+temp = sum(A,1);
 for i=1:n
-    U(:,i) = U(:,i)./temp(i);
+    A(:,i) = A(:,i)./temp(i);
 end
 
 L=zeros(c,n);
 
 %先计算beta
-% beta=sum(sum(data,2)/s)/n;
-% beta=2*beta^2;
-beta=2;
+beta=sum(sum(data,2)/s)/n;
+beta=2*(beta)^(1/n);
+% beta=2;
 %计算初始聚类中心
 V(c,s)=0;
 
@@ -24,11 +24,11 @@ V(c,s)=0;
 t=0;
 objFcn=zeros(1,T);
 tmp1=0;
-tmp2=0;
 while(t<T)
     t=t+1;
+    
     %更新聚类中心
-    V=U*data./(sum(U ,2)*ones(1,s));
+    V=A*data./(sum(A ,2)*ones(1,s));
     
     %计算第i个样本与第k类的距离
     for i=1:n
@@ -43,21 +43,20 @@ while(t<T)
             num=num+exp(-L(k,i)^2/beta);
         end
         for k=1:c
-            U(k,i)=exp(-L(k,i)^2/beta)/num;
+            A(k,i)=exp(-L(k,i)^2/beta)/num;
         end
     end
     
     %计算目标函数的值
-    tmp2=sum(sum(-U.*log(U)));
+    tmp2=sum(sum(-A.*log(A)));
     
     if (abs(tmp2-tmp1)<epsm)
         break
     end
     tmp1=tmp2;
     objFcn(t)=tmp1;
-%     objFcn(2,t)=sum(sum(U.*log(U)));
 end
 %绘制结果
-C=F2Q(U);
-myplot(U,objFcn);
+C=F2Q(A);
+myplot(A,objFcn);
 end
